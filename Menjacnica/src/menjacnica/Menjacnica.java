f8162ceeb195c43f35e68057a097083437791758
jahicDario <dariojahic95@gmail.com>
2016-03-19 18:14:46 +0100

    Dodato da klasa Menjacnica implementira interfejs MenjacnicaInterfejs (metode nisu implementirane).

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,10 +1,29 @@
 package menjacnica;
+
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
 
 	private LinkedList<Valuta> valuta;
 	private GregorianCalendar datum;
-	
+
+	@Override
+	public void dodajKursaValute(GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public void brisanjeKursaValuta(GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public Valuta pronalazenjeKursaValute(GregorianCalendar datum) {
+		
+		return null;
+	}
+
 }
