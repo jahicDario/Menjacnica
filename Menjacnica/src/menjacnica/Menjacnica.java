e2537e2fcb3f9eeb6c6409f80242c5fdb86ee47b
jahicDario <dariojahic95@gmail.com>
2016-03-19 17:31:57 +0100

    Dodate klase sa atributima i generisane get i set metode.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,10 @@
+package menjacnica;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+public class Menjacnica {
+
+	private LinkedList<Valuta> valuta;
+	private GregorianCalendar datum;
+	
+}
